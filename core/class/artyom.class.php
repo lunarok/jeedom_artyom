509528caa60d978a7c539b6766c5472d39093b11
Cédric Dupont <cedric.dupont@bull.net>
2016-03-17 19:18:14 +0100

    Widget preToHtml

--- core/class/artyom.class.php
+++ core/class/artyom.class.php
@@ -21,6 +21,8 @@ require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
 class artyom extends eqLogic {
 
+  public static $_widgetPossibility = array('custom' => true);
+
   public static function health() {
     $return = array();
     if (strpos(network::getNetworkAccess('external'),'https') !== false) {
@@ -48,107 +50,59 @@ class artyom extends eqLogic {
     }
   }
 
-  public static function cronHourly() {
-      foreach (eqLogic::byType('artyom', true) as $artyom) {
-        $mc = cache::byKey('artyomWidgetdashboard' . $artyom->getId());
-        $mc->remove();
-        $artyom->toHtml('dashboard');
-        $mc = cache::byKey('artyomWidgetmobile' . $artyom->getId());
-        $mc->remove();
-        $artyom->toHtml('mobile');
-        $artyom->refreshWidget();
-      }
-  }
+
 
   public function postUpdate() {
-      foreach (eqLogic::byType('artyom', true) as $artyom) {
-        $mc = cache::byKey('artyomWidgetdashboard' . $artyom->getId());
-        $mc->remove();
-        $artyom->toHtml('dashboard');
-        $mc = cache::byKey('artyomWidgetmobile' . $artyom->getId());
-        $mc->remove();
-        $artyom->toHtml('mobile');
-        $artyom->refreshWidget();
-      }
-
-      $text = $this->getCmd(null, 'tts');
-  		if (!is_object($text)) {
-  			$text = new artyomCmd();
-  			$text->setLogicalId('tts');
-  			$text->setIsVisible(0);
-  			$text->setName(__('Message', __FILE__));
-  		}
-  		$text->setType('action');
-  		$text->setSubType('message');
-  		$text->setEqLogic_id($this->getId());
-  		$text->save();
+    $text = $this->getCmd(null, 'tts');
+    if (!is_object($text)) {
+      $text = new artyomCmd();
+      $text->setLogicalId('tts');
+      $text->setIsVisible(0);
+      $text->setName(__('Message', __FILE__));
+    }
+    $text->setType('action');
+    $text->setSubType('message');
+    $text->setEqLogic_id($this->getId());
+    $text->save();
   }
 
 
   public function toHtml($_version = 'dashboard') {
-
-    $mc = cache::byKey('artyomWidget' . $_version . $this->getId());
-    if ($mc->getValue() != '') {
-      return $mc->getValue();
+    $replace = $this->preToHtml($_version);
+    if (!is_array($replace)) {
+      return $replace;
     }
-    if ($this->getIsEnable() != 1) {
-            return '';
-        }
-        if (!$this->hasRight('r')) {
-            return '';
-        }
-        $_version = jeedom::versionAlias($_version);
-        if ($this->getDisplay('hideOn' . $_version) == 1) {
-            return '';
-        }
-        $vcolor = 'cmdColor';
-        if ($_version == 'mobile') {
-            $vcolor = 'mcmdColor';
-        }
-        $parameters = $this->getDisplay('parameters');
-        $cmdColor = ($this->getPrimaryCategory() == '') ? '' : jeedom::getConfiguration('eqLogic:category:' . $this->getPrimaryCategory() . ':' . $vcolor);
-        if (is_array($parameters) && isset($parameters['background_cmd_color'])) {
-            $cmdColor = $parameters['background_cmd_color'];
-        }
+    $version = jeedom::versionAlias($_version);
+    if ($this->getDisplay('hideOn' . $version) == 1) {
+      return '';
+    }
 
-        if (($_version == 'dview' || $_version == 'mview') && $this->getDisplay('doNotShowNameOnView') == 1) {
-            $replace['#name#'] = '';
-            $replace['#object_name#'] = (is_object($object)) ? $object->getName() : '';
-        }
-        if (($_version == 'mobile' || $_version == 'dashboard') && $this->getDisplay('doNotShowNameOnDashboard') == 1) {
-            $replace['#name#'] = '<br/>';
-            $replace['#object_name#'] = (is_object($object)) ? $object->getName() : '';
-        }
+    foreach ($this->getCmd('info') as $cmd) {
+      $replace['#' . $cmd->getLogicalId() . '_history#'] = '';
+      $replace['#' . $cmd->getLogicalId() . '_id#'] = $cmd->getId();
+      $replace['#' . $cmd->getLogicalId() . '#'] = $cmd->execCmd();
+      $replace['#' . $cmd->getLogicalId() . '_collect#'] = $cmd->getCollectDate();
+      if ($cmd->getIsHistorized() == 1) {
+        $replace['#' . $cmd->getLogicalId() . '_history#'] = 'history cursor';
+      }
+    }
 
-        if (is_array($parameters)) {
-            foreach ($parameters as $key => $value) {
-                $replace['#' . $key . '#'] = $value;
-            }
-        }
+    if (!config::byKey('internalPort')) {
+      $url = config::byKey('internalProtocol') . config::byKey('internalAddr') . config::byKey('internalComplement') . '/core/api/jeeApi.php?api=' . config::byKey('api');
+    } else {
+      $url = config::byKey('internalProtocol') . config::byKey('internalAddr'). ':' . config::byKey('internalPort') . config::byKey('internalComplement') . '/core/api/jeeApi.php?api=' . config::byKey('api');
+    }
 
-        if (!config::byKey('internalPort')) {
-          $url = config::byKey('internalProtocol') . config::byKey('internalAddr') . config::byKey('internalComplement') . '/core/api/jeeApi.php?api=' . config::byKey('api');
-        } else {
-          $url = config::byKey('internalProtocol') . config::byKey('internalAddr'). ':' . config::byKey('internalPort') . config::byKey('internalComplement') . '/core/api/jeeApi.php?api=' . config::byKey('api');
-        }
+    $lang = explode('_',config::byKey('language'));
 
-        $lang = explode('_',config::byKey('language'));
+    $replace['#keyend#'] = $this->getConfiguration('keyend');
+    $replace['#keyword#'] = $this->getConfiguration('keyword');
+    $replace['#jeedom#'] = $url;
+    $replace['#lang#'] = $lang[0];
+    $replace['#continuous#'] = $this->getConfiguration('continuous');
 
-        $replace = array(
-            		'#name#' => $this->getName(),
-                	'#id#' => $this->getId(),
-                  '#keyend#' => $this->getConfiguration('keyend'),
-                  '#keyword#' => $this->getConfiguration('keyword'),
-                  '#jeedom#' => $url,
-                  '#lang#' => $lang[0],
-                  '#continuous#' => $this->getConfiguration('continuous'),
-                	'#background_color#' => $this->getBackgroundColor(jeedom::versionAlias($_version)),
-                	'#eqLink#' => ($this->hasRight('w')) ? $this->getLinkToConfiguration() : '#',
-            	);
-
-      $html = template_replace($replace, getTemplate('core', $_version, 'artyom', 'artyom'));
-      cache::set('artyomWidget' . $_version . $this->getId(), $html, 0);
-      return $html;
+    $html = template_replace($replace, getTemplate('core', $_version, 'artyom', 'artyom'));
+    return $html;
   }
 
 }
@@ -165,10 +119,10 @@ class artyomCmd extends cmd {
     $text = trim($_options['title'] . ' ' . $_options['message']);
     event::add('artyom::stackData', $text);
     /*$('body').on('artyom::stackData', function (_event,param) {
-           //var _cmd = JSON.parse(param);
-           artyom.say(data);
-       });*/
-    }
+    //var _cmd = JSON.parse(param);
+    artyom.say(data);
+  });*/
+}
 
 }
 
