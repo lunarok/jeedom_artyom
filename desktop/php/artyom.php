2484905ed909b88cce87026383f0cebd04628abe
lunarok <lunarok@gmail.com>
2016-08-07 14:21:57 +0200

    decalage

--- desktop/php/artyom.php
+++ desktop/php/artyom.php
@@ -100,7 +100,7 @@ $eqLogics = eqLogic::byType('artyom');
               </div>
             </div>
             <div class="form-group">
-              <label class="col-sm-2 control-label" ></label>
+              <label class="col-sm-3 control-label" ></label>
               <div class="col-sm-8">
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
